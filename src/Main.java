f0b190904f24bccb17fafab3387945958122df77
astal <astalbox@gmail.com>
2026-05-28 10:24:17 +0300

    Test pull request

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
-void main() {
-    System.out.println("Hello world!!!");
+class App {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        int test = 1;
+    }
 }
