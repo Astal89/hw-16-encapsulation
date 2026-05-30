b252d3c4250fdc3ec31fbbce9783c90a6778de77
astal <astalbox@gmail.com>
2026-05-30 12:11:26 +0300

    Test pull request

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 class App {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello");
         byte a = 1;
         byte b = 1;
         byte c = (byte)(a + b);
