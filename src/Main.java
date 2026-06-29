e11d359c034ba29f5ece53b54262991cb4f197d0
astal <astalbox@gmail.com>
2026-06-29 14:29:46 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
+import java.util.Objects;
+
 class App {
     public static void main(String[] args) {
-        System.out.println("hello");
-        byte a = 1;
-        byte b = 1;
-        byte c = (byte)(a + b);
+        System.out.println("Hello world!");
     }
 }
